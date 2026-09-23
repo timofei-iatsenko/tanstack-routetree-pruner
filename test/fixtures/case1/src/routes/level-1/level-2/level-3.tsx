40203c50600028029ebe3e00c632a2bbb433d5ef
iatsenko <1586852+timofei-iatsenko@users.noreply.github.com>
2026-09-23 11:53:59 +0200

    fix

--- test/fixtures/case1/src/routes/level-1/level-2/level-3.tsx
+++ test/fixtures/case1/src/routes/level-1/level-2/level-3.tsx
@@ -1,17 +1,5 @@
 import { createFileRoute } from "@tanstack/react-router";
 
-declare module "@tanstack/router-core" {
-  interface RouteExtensions<TId, TFullPath> {
-    __bla: string;
-  }
-}
-
 export const Route = createFileRoute("/level-1/level-2/level-3")({
   component: () => {},
 });
-
-Route.__bla;
-
-// type t = typeof Route;
-//
-// type t2 = t['']
